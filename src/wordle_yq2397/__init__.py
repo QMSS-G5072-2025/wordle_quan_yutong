bf5bd1235873c7ea2bdba0084214c9cef3f6106a
yutongquan <yutongq@outlook.com>
2025-10-29 14:48:42 -0400

    add wordle functions

--- src/wordle_yq2397/__init__.py
+++ src/wordle_yq2397/__init__.py
@@ -1,3 +1,23 @@
+"""
+Wordle game package.
+
+A package for playing Wordle with validation, checking, and scoring functions.
+"""
+
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("wordle_yq2397")
+
+from wordle_yq2397.wordle_yq2397 import (
+    validate_guess,
+    check_guess,
+    is_valid_word,
+    calculate_game_score,
+)
+
+__all__ = [
+    "validate_guess",
+    "check_guess",
+    "is_valid_word",
+    "calculate_game_score",
+]
